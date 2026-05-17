7e5abdccf1cc22de3136d51772e407d5de5a398c
Monica Davila <monica.davila510@gmail.com>
2026-05-17 10:41:16 -0500

    add filter functionality to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import TaskList from "./components/TaskList/TaskList.tsx";
+import { useState } from "react";
+import TaskList from "./components/TaskList/TaskList";
+import TaskFilter from "./components/TaskFilter/TaskFilter";
 
 function App() {
+  const [filter, setFilter] = useState("all");
+
   return (
     <div>
       <h1>Task Manager</h1>
+
+      <TaskFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+
       <TaskList />
     </div>
   );
